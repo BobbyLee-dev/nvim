04d20eb69bfc2b0c1af11d5087f69908ec4411d4
Bobby Lee <bobbylee.dev@gmail.com>
2025-09-21 14:05:38 -0700

    adds pretty php

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,7 +2,7 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      php = { "phpcbf" },
+      php = { "phpcbf", "pretty_php" },
       css = { "stylelint" },
       scss = { "prettier", "stylelint" }, -- use if stylelint rules are crazy and don't want to manually fix.
       -- scss = { "prettier" }, -- use when don't mind fixing some lints manually.
@@ -19,6 +19,16 @@ return {
           "phpcs.xml",
           "phpcs.xml.dist",
           "composer.json",
+        }),
+      },
+      pretty_php = {
+        command = "pretty-php",
+        -- tell PrettyPHP to use the WordPress preset
+        args = { "--preset=wordpress", "$FILENAME" },
+        stdin = false,
+        cwd = require("conform.util").root_file({
+          "composer.json",
+          ".git",
         }),
       },
     },
